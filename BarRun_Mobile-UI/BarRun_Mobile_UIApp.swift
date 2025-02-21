2323d226f5676d685be4db2c4c203fa742634f7d
JThoe26 <jackson.thoe@mnsu.edu>
2025-02-20 22:23:05 -0600

    Initial Commit

---
+++ BarRun_Mobile-UI/BarRun_Mobile_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarRun_Mobile_UIApp.swift
+//  BarRun_Mobile-UI
+//
+//  Created by Jackson Thoe on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BarRun_Mobile_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarRun_Mobile-UITests/BarRun_Mobile_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  BarRun_Mobile_UITests.swift
+//  BarRun_Mobile-UITests
+//
+//  Created by Jackson Thoe on 2/20/25.
+//
+
+import Testing
+@testable import BarRun_Mobile_UI
+
+struct BarRun_Mobile_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
